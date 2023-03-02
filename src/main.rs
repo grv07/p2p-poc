18e984df9a875335f72430ea991496d51f44b110
Gaurav Tyagi <grvtyagi22@gmail.com>
2023-03-02 11:18:09 +0530

    add basic structure for data state

---
+++ src/main.rs
@@ -0,0 +1,37 @@
+type Recipes = Vec<Recipe>;
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Recipe {
+    id: usize,
+    name: String,
+    ingredients: String,
+    instructions: String,
+    public: bool,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+enum ListMode {
+    ALL,
+    One(String),
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct ListRequest {
+    mode: ListMode,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct ListResponse {
+    mode: ListMode,
+    data: Recipes,
+    receiver: String,
+}
+
+enum EventType {
+    Response(ListResponse),
+    Input(String),
+}
+
+fn main() {
+    println!("Hello, world!");
+}
