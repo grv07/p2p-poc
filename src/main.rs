27cdfe51fb29785dd45c10822da3a5859e7fda1c
Gaurav Tyagi <grvtyagi22@gmail.com>
2023-03-02 11:20:11 +0530

    add the static states

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+const STORAGE_FILE_PATH: &str = "./recipes.json";
+
+type Result<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync + 'static>>;
+
+static KEYS: Lazy<identity::Keypair> = Lazy::new(|| identity::Keypair::generate_ed25519());
+static PEER_ID: Lazy<PeerId> = Lazy::new(|| PeerId::from(KEYS.public()));
+static TOPIC: Lazy<Topic> = Lazy::new(|| Topic::new("recipes"));
+
 type Recipes = Vec<Recipe>;
 
 #[derive(Debug, Serialize, Deserialize)]
