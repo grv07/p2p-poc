80dbf10638a14beb906433619d48f190e2fa3a75
Gaurav Tyagi <grvtyagi22@gmail.com>
2023-03-03 12:33:00 +0530

    fix some of imports and run basic example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use libp2p::{
+    core::transport::upgrade,
+    floodsub::{Floodsub, Topic},
+    identity,
+    mdns::{Mdns, MdnsEvent},
+    mplex,
+    noise::{Keypair, NoiseConfig, X25519Spec},
+    swarm::{NetworkBehaviour, Swarm, SwarmBuilder},
+    tcp::TokioTcpConfig,
+    PeerId, Transport,
+};
+// use libp2p::{swarm::SwarmBuilder, Swarm};
+use log::info;
+use once_cell::sync::Lazy;
+use serde::{Deserialize, Serialize};
+use tokio::{fs, io::AsyncBufReadExt, sync::mpsc};
+
 const STORAGE_FILE_PATH: &str = "./recipes.json";
 
 type Result<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync + 'static>>;
@@ -40,6 +57,70 @@ enum EventType {
     Input(String),
 }
 
-fn main() {
-    println!("Hello, world!");
+#[derive(NetworkBehaviour)]
+struct RecipeBehaviour {
+    floodsub: Floodsub,
+    mdns: TokioMdns,
+    #[behaviour(ignore)]
+    response_sender: mpsc::UnboundedSender<ListResponse>,
+}
+
+// impl NetworkBehaviour<MdnsEvent> for RecipeBehaviour {
+//     fn on(&mut self, event: MdnsEvent) {
+//         match event {
+//             MdnsEvent::Discovered(discovered_list) => {
+//                 for (peer, _addr) in discovered_list {
+//                     self.floodsub.add_node_to_partial_view(peer);
+//                 }
+//             }
+//             MdnsEvent::Expired(expired_list) => {
+//                 for (peer, _addr) in expired_list {
+//                     if !self.mdns.has_node(&peer) {
+//                         self.floodsub.remove_node_from_partial_view(&peer);
+//                     }
+//                 }
+//             }
+//         }
+//     }
+// }
+
+#[tokio::main]
+async fn main() {
+    pretty_env_logger::init();
+
+    info!("Peer Id: {}", PEER_ID.clone());
+    let (response_sender, mut response_rcv) = mpsc::unbounded_channel();
+
+    let auth_keys = Keypair::<X25519Spec>::new()
+        .into_authentic(&KEYS)
+        .expect("can create auth keys");
+
+    let transp = TokioTcpConfig::new()
+        .upgrade(upgrade::Version::V1)
+        .authenticate(NoiseConfig::xx(auth_keys).into_authenticated())
+        .multiplex(mplex::MplexConfig::new())
+        .boxed();
+
+    let mut behaviour = RecipeBehaviour {
+        floodsub: Floodsub::new(PEER_ID.clone()),
+        mdns: Mdns::new().expect("can create mdns"),
+        response_sender,
+    };
+
+    behaviour.floodsub.subscribe(TOPIC.clone());
+
+    let mut swarm = SwarmBuilder::new(transp, behaviour, PEER_ID.clone())
+        .executor(Box::new(|fut| {
+            tokio::spawn(fut);
+        }))
+        .build();
+
+    let mut stdin = tokio::io::BufReader::new(tokio::io::stdin()).lines();
+    Swarm::listen_on(
+        &mut swarm,
+        "/ip4/0.0.0.0/tcp/0"
+            .parse()
+            .expect("can get a local socket"),
+    )
+    .expect("swarm can be started");
 }
